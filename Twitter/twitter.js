69b7f24c556e1064cc1adda4507c64c132e0128a
ZivKa <zivkalderon95@gmail.com>
2018-07-15 06:39:32 +0300

    Update twitter.js

--- Twitter/twitter.js
+++ Twitter/twitter.js
@@ -1,14 +1,10 @@
 
 const Twit = require('twit');
 const config = {
-    consumer_key: 'ZFd2vmgHT0PwIXpInLNUY7en7',
-    consumer_secret: 'DkhOY2IIUAeADlQwSYAw6915nJe7pYzEEMBXiV7Jizgg5vDTx6',
-    access_token: '816669683420168192-GITXTydXdsf9Iaz1UsKnzbPxJZzv05Z',
-    access_token_secret: 'mAYs2gKDtVxldbk0feQqBJ5SuvmFb5i4Sj0OZ2Ax6hVCX'
-    // consumer_key: process.env.CONSUMER_KEY,
-    // consumer_secret: process.env.CONSUMER_SECRET,
-    // access_token: process.env.ACCESS_TOKEN,
-    // access_token_secret: process.env.ACCESS_TOKEN_SECRET
+    consumer_key: process.env.CONSUMER_KEY,
+    consumer_secret: process.env.CONSUMER_SECRET,
+    access_token: process.env.ACCESS_TOKEN,
+    access_token_secret: process.env.ACCESS_TOKEN_SECRET
 };
 let twit = new Twit(config);
 
