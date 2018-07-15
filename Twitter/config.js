3e2a55fddc92d986a623309bec99b1cded7cd8d3
ZivKa <zivkalderon95@gmail.com>
2018-07-15 07:52:55 +0300

    Update config.js

--- Twitter/config.js
+++ Twitter/config.js
@@ -5,8 +5,8 @@
  * access_token_secret
  */
 module.exports = {
-  consumer_key: 'ZFd2vmgHT0PwIXpInLNUY7en7',
-  consumer_secret: 'DkhOY2IIUAeADlQwSYAw6915nJe7pYzEEMBXiV7Jizgg5vDTx6',
-  access_token: '816669683420168192-GITXTydXdsf9Iaz1UsKnzbPxJZzv05Z',
-  access_token_secret: 'mAYs2gKDtVxldbk0feQqBJ5SuvmFb5i4Sj0OZ2Ax6hVCX'
+  consumer_key: '',
+  consumer_secret: '',
+  access_token: '',
+  access_token_secret: ''
 }
