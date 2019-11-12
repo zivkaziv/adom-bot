9cc4ab96d5f0da5f2b88329b0a0cb5811c98694b
zivkaziv <Z15194806k>
2019-11-12 09:44:53 +0200

    fix alerts

--- index.js
+++ index.js
@@ -7,40 +7,66 @@ var interval = 5000;
 var restarted = true;
 const twitMessager = require('./Twitter/twitter');
 const telegram = require('./Telegram/telegram');
+const axios = require('axios')
 
 // Define polling function
-let poll = () => {
+let poll = async() => {
+    try{
+        const res = await axios.get('https://alerts.ynet.co.il/alertsRss/YnetPicodeHaorefAlertFiles.js?callback=jsonCallback');
+        //response looks like - "jsonCallback({"alerts": {"items": []}});"
+        //trim it to json
+        const dataStr = res.data.replace('jsonCallback(','').slice(0,-2);
+        console.log(dataStr);
+        const data = JSON.parse(dataStr);
+        if(data.alerts && data.alerts.items.length){
+            //{"alerts": {"items": [{"item": {"guid": "b3419d33-c1c8-4009-a08c-fd58d9a30ea9","pubdate": "09:27","title": "מפלסים","description": "היכנסו למרחב המוגן","link": ""}},{"item": {"guid": "b3419d33-c1c8-4009-a08c-fd58d9a30ea9","pubdate": "09:27","title": "מטווח ניר עם","description": "היכנסו למרחב המוגן","link": ""}},{"item": {"guid": "978cd0b4-2e75-4358-8094-bada5662b588","pubdate": "09:26","title": "מבטחים עמיעוז ישע","description": "היכנסו למרחב המוגן","link": ""}},{"item": {"guid": "978cd0b4-2e75-4358-8094-bada5662b588","pubdate": "09:26","title": "צוחר ואוהד","description": "היכנסו למרחב המוגן","link": ""}}]}}
+            console.log(data.alerts)
+            const alertZones = data.alerts.items.map(item => {
+                return item.item.title;
+            })
+            const text = generateText(alertZones);
+            console.log(text);
+            createTelegramMessage(text);
+        }else{
+            console.log('No alerts');
+        }
+    }catch(err){
+        console.log(err);
+    }
+    setTimeout(poll, interval);
     // Get currently active rocket alert zones as an array of zone codes
     // Example response: ["גולן 1", "חיפה 75", "שפלה 182"]
-    pikudHaoref.getActiveRocketAlertZones(function (err, alertZones) {
-        // Schedule polling in X millis
-        setTimeout(poll, interval);
+    // pikudHaoref.getActiveRocketAlertZones(function (err, alertZones) {
+    //     // Schedule polling in X millis
+    //     setTimeout(poll, interval);
 
-        // Log errors
-        if (err) {
-            return console.log('Retrieving active rocket alert zones failed: ', err);
-        }
-        // Alert zones header
-        console.log('Currently active rocket alert zones:');
+    //     // Log errors
+    //     if (err) {
+    //         return console.log('Retrieving active rocket alert zones failed: ', err);
+    //     }
+    //     // Alert zones header
+    //     console.log('Currently active rocket alert zones:');
 
-        // Log the alert zones (if any)
-        console.log(alertZones);
-        if(alertZones.length > 0 || restarted){
-            if(restarted){
-                restarted = false;
-                //alertZones.push('Restarted');
-            }
-            let text = generateText(alertZones);
-            //createTweet(text);
-            createTelegramMessage(text);
-        }
-        // Line break for readability
-        console.log();
-    });
+    //     // Log the alert zones (if any)
+    //     console.log(alertZones);
+    //     if(alertZones.length > 0 || restarted){
+    //         if(restarted){
+    //             restarted = false;
+    //             //alertZones.push('Restarted');
+    //         }
+    //         let text = generateText(alertZones);
+    //         //createTweet(text);
+    //         createTelegramMessage(text);
+    //     }
+    //     // Line break for readability
+    //     console.log();
+    // });
 };
 
 let generateText = (alertZones) =>{
-    return alertZones.join(' וגם ');
+    return `צבע אדום ב
+${alertZones.join(', ')}
+תעיפו את עצמכם למרחבים המוגנים...`;
 };
 
 let createTweet = (tweetText) =>{
