9d492194d78aef1bff3f925d8456652063d3b596
ZivKa <zivkalderon95@gmail.com>
2019-12-18 14:57:44 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 'use strict';
 
-// const pikudHaoref = require('pikud-haoref-api');
+// const pikudHaoref = require('pikud-haoref-api');//not in used
 
 // Set polling interval in millis
 var interval = 10000;
